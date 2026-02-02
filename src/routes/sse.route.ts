187906758764064534e14c9fc43c0cdcc6214a3d
Arup Basak <email.arupbasak@gmail.com>
2026-02-02 13:25:47 +0530

    v1.0.4

--- src/routes/sse.route.ts
+++ src/routes/sse.route.ts
@@ -1,4 +1,4 @@
-import { Router, Response } from "express";
+import { Router } from "express";
 import { verifyToken } from "../middleware/auth.middleware";
 import { MemoryService as Memory } from "../service/memory.service";
 import { NotificationService } from "../service/notification.service";
@@ -6,28 +6,31 @@ import { NotificationService } from "../service/notification.service";
 const router = Router();
 
 router.get("/events", verifyToken, async (req, res) => {
-	res.setHeader("Content-Type", "text/event-stream");
-	res.setHeader("Cache-Control", "no-cache");
-	res.setHeader("Connection", "keep-alive");
-	res.flushHeaders();
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+  res.flushHeaders();
 
-	const userId = req.user?.id;
+  const userId = req.user?.id;
+  const username = req.user?.username || req.user?.name;
 
-	if (!userId) {
-		return;
-	}
+  console.log(req.user)
 
-	Memory.addClient(userId, res);
+  if (!userId) {
+    return;
+  }
 
-	res.write(
-		`event: ping\n` +
-			`data: ${JSON.stringify({ total_users: Memory.totalClients() })}\n\n`,
-	);
-	NotificationService.sendUnsentNotifications(userId);
+  Memory.addClient(userId, res);
 
-	req.on("close", async () => {
-		Memory.removeClient(userId);
-	});
+  res.write(
+    `event: ping\n` +
+      `data: ${JSON.stringify({ total_users: Memory.totalClients(), user: userId, username })}\n\n`
+  );
+  NotificationService.sendUnsentNotifications(userId);
+
+  req.on("close", async () => {
+    Memory.removeClient(userId);
+  });
 });
 
 export default router;
